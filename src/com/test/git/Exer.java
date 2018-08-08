57657788f51c6df9b2586e8844fda5f978b6ee66
jamie <384505109@qq.com>
2018-08-09 00:38:33 +0800

    clone

--- src/com/test/git/Exer.java
+++ src/com/test/git/Exer.java
@@ -3,7 +3,6 @@ package com.test.git;
 public class Exer {
 
 	public static void main(String[] args) {
-		System.out.println("qqqqqqqq");
-		System.out.println("233232323");
+		System.out.println("俱往矣，数风流人物，还看今朝！");
 	}
 }
